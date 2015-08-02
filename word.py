3528fd7420845a90d99a8aa39cb1ec224bb07988
=epogrebnyak <e.pogrebnyak@gmail.com>
2015-08-02 14:10:38 +0300

    delimiting

--- word.py
+++ word.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
+"""Dumps data from tables in Word document to csv file.   
+
 API:
 https://msdn.microsoft.com/en-us/library/office/ff837519.aspx
 
@@ -15,6 +16,11 @@ import os
 import csv
 import re
 import sqlite3
+
+#______________________________________________________________________________
+#
+#  Apllication management
+#______________________________________________________________________________
 
 def open_ms_word():
     word = win32.Dispatch("Word.Application")
@@ -32,6 +38,32 @@ def open_doc(path, word):
 def get_table_count(doc):
     return doc.Tables.count    
 
+#______________________________________________________________________________
+#
+#  Utilities
+#______________________________________________________________________________
+
+
+def change_extension(p, newext):
+    if not newext.startswith("."):
+        newext = "." + newext
+    return os.path.splitext(p)[0] + newext
+
+def get_basename(p):
+    return os.path.splitext(p)[0]
+
+def is_year(s):
+    try:
+        int(s)
+        return True        
+    except:
+        return False
+
+#______________________________________________________________________________
+#
+#  Table iterators
+#______________________________________________________________________________
+
 def get_cell_value(table, i, j):
     try:
        return table.Cell(Row = i, Column= j).Range.Text
@@ -39,11 +71,11 @@ def get_cell_value(table, i, j):
        return ""
       
 def get_filtered_cell_value(table, i, j):
-     cell_value = get_cell_value(table, i, j)
      replacements = [('\r\x07', '')    # delete this symbol
                    , ('\x0c',   ' ')   # sub with space
                    , ('\x0b',   ' ')   # sub with space
                    , ('\r',     ' ')]  # sub with space
+     cell_value = get_cell_value(table, i, j)
      for a, b in replacements: 
           cell_value = cell_value.replace(a, b)
      return cell_value.strip()     
@@ -59,25 +91,11 @@ def row_iter(table):
         for j in range(1,table.columns.count + 1):
             row = row + [get_filtered_cell_value(table, i, j)]  
         yield row
-       
-def dump_table_to_csv(table, csv_filename):
-    iterable = row_iter(table)   
-    dump_iter_to_csv(iterable, csv_filename)
 
-def dump_iter_to_csv(iterable, csv_filename):
-    with open(csv_filename, 'w') as csvfile:
-        spamwriter = csv.writer(csvfile,  delimiter='\t', lineterminator='\n')
-        for row in iterable:        
-             spamwriter.writerow(row) 
-
-def get_basename(p):
-    return os.path.splitext(os.path.basename(p))[0]
-
-def dump_all_tables(p, word):
-    doc = open_doc(p, word) 
-    for i, table in enumerate(doc.Tables):
-        csv_filename = get_basename(p) + "_" + str(i) + ".csv"
-        dump_table_to_csv(table, csv_filename)    
+#______________________________________________________________________________
+#
+#  Document-level iterators
+#______________________________________________________________________________
 
 def query_all_tables(p, func = cell_iter):
     word = open_ms_word()
@@ -85,7 +103,8 @@ def query_all_tables(p, func = cell_iter):
     for table in doc.Tables:
         yield func(table)    
     close_ms_word(word)
-    
+
+
 def yield_continious_rows(p):
     for y in query_all_tables(p, func = row_iter):
         for row in y:
@@ -95,31 +114,73 @@ def yield_continious_cells(p):
     for y in query_all_tables(p, func = cell_iter):
         for cell in y:
            yield cell           
- 
-def is_year(s):
-    try:
-        int(s)
-        return True        
-    except:
-        return False
-        
-def dump_doc_to_single_csv_file(p):
-    csv_filename = get_basename(p) + ".csv"
-    many_rows_iter = yield_continious_rows(p)
-    dump_iter_to_csv(many_rows_iter, csv_filename)    
-        
+  
+#______________________________________________________________________________
+#
+#  CSV IO functions
+#______________________________________________________________________________
+
+def dump_iter_to_csv(iterable, csv_filename):
+    with open(csv_filename, 'w') as csvfile:
+        spamwriter = csv.writer(csvfile,  delimiter='\t', lineterminator='\n')
+        for row in iterable:        
+             spamwriter.writerow(row) 
+
 def yield_csv_rows(path):
     with open(path, 'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter='\t', lineterminator='\n')
         for row in spamreader:
             yield row
+
+#______________________________________________________________________________
+#
+#  CSV IO functions
+#______________________________________________________________________________
+
+     
+def dump_table_to_csv(table, csv_filename):
+    iterable = row_iter(table)   
+    dump_iter_to_csv(iterable, csv_filename)
+
+
+def dump_all_tables(p, word):
+    doc = open_doc(p, word) 
+    for i, table in enumerate(doc.Tables):
+        csv_filename = get_basename(p) + "_" + str(i) + ".csv"
+        dump_table_to_csv(table, csv_filename)    
+
+def dump_doc_to_single_csv_file(p):
+    csv_filename = change_extension(p, ".csv")
+    #              get_basename(p) + ".csv"
+    many_rows_iter = yield_continious_rows(p)
+    dump_iter_to_csv(many_rows_iter, csv_filename) 
+    return csv_filename
+        
+#______________________________________________________________________________
+#
+#  File management 
+#______________________________________________________________________________
         
 def make_headers(p):
-    out = get_basename(p) + "headers.txt"
+    """Makes a list of docfile table headers and footers in txt file.
+    Used to review file contents and manually make label dictionaries""" 
+    
+    out = get_basename(p) + "_headers.txt"
     with open(out, "w") as file:
        for row in yield_csv_rows(p):
            if not is_year(row[0]) and len(row[0]) > 0:
                 file.write(row[0] + "\n")
+    return out
+                
+#______________________________________________________________________________
+#
+#  Make CSV with labelled rows and filtered values. 
+#______________________________________________________________________________
+                
+def make_labelled_csv(source_csv_filename, output_csv_filename, 
+                                          full_dict, unit_dict): 
+    gen = labelled_row_iter(source_csv_filename, label_dict, sec_label_dict)
+    dump_iter_to_csv(gen, output_csv_filename)
 
 def get_label(text, lab_dict):
     for pat in lab_dict.keys():
@@ -143,7 +204,7 @@ def labelled_row_iter(path, full_dict, unit_dict):
              else:
                  yield(labels + row)
          else:
-             pass # nothing in this row
+             pass # there is nothing in this row, len is 0
          
 def test_row_split():   
     row = [2007, 6716.2, 897.6, 1414.4, 1744.1, 2660.1, 255.3, 298.0, 344.3, 364.5, 
@@ -167,7 +228,7 @@ def test_filter_comment():
     assert filter_comment("20.5 3)") == "20.5"
     
     
-def filter_value2(text):
+def filter_value(text):
    text = text.replace(",",".")
    if ')' in text:
        text = filter_comment(text)
@@ -176,11 +237,18 @@ def filter_value2(text):
    else:
        return None
 
+#______________________________________________________________________________
+#
+#  Write labelled CSV to database 
+#______________________________________________________________________________
+
+DB_FILE = 'kep.sqlite'
+                
 def yield_vars(path):      
     for row in yield_csv_rows(path):
         var_name = row[0] + "_" + row[1]
         
-        mod_row = [filter_value2(x) for x in row[2:]]        
+        mod_row = [filter_value(x) for x in row[2:]]        
         y, a, qs, ms = split_row_by_periods(mod_row)
         yield var_name, int(y), a, qs, ms 
         
@@ -193,10 +261,9 @@ def push_quarter(cursor, var_name, year, quarter, val):
 def push_monthly(cursor, var_name, year, month, val):
     cursor.execute("INSERT OR REPLACE INTO monthly VALUES (?, ?, ?, ?)", (var_name, year, month, val))
 
-
 def push_to_database(path):
 
-    conn = sqlite3.connect('kep.sqlite')
+    conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     
     for vn, y, a, qs, ms in yield_vars(path):
@@ -215,19 +282,52 @@ def push_to_database(path):
     # Just be sure any changes have been committed or they will be lost.
     conn.close()
     
+#______________________________________________________________________________
+#
+#  Read specification
+#______________________________________________________________________________
+ 
+import yaml as ya
+
+
+def load_spec(p):
+    """Wrapper for load_spec_from_yaml()"""
+    f = get_basename(p) + "_spec.txt"
+    return load_spec_from_yaml(f
+
+def load_spec_from_yaml(p):
+    """Returns two dictionaries of label specifications. Unpacking:
+       full_dict, unit_dict = load_spec_from_yaml(p)
+    """
+    spec = [d for d in ya.load_all(p)]
+    return spec[1], spec[0]     
+
+               
+#______________________________________________________________________________
+#
+#  Batch jobs 
+#______________________________________________________________________________
+                
+def make_reabable_csv(p):
+    print ("File:", p)
     
+    c = dump_doc_to_single_csv_file(p)
+    print ("Finished writing:", c)
+    
+    h = make_headers(c)
+    print ("Finished writing:", h)
+    
+    return c
+                
 if __name__ == "__main__":
     test_row_split()
     test_filter_comment()
-        
-    p1 = os.path.abspath("tab.doc")
-    p2 = os.path.abspath("1-07.doc")
-    #dump_doc_to_single_csv_file(p2)
-    #dump_doc_to_single_csv_file(p1)
-    c1 = os.path.abspath("tab.csv")
-    c2 = os.path.abspath("1-07.csv")
-    t2 = "1-07.txt" 
     
+    src_doc = ["1-07.doc", "ind06/tab.doc", "minitab/minitab.doc"] 
+    p = os.path.abspath(src_doc[0])
+    c = make_reabable_csv(p)
+    
+    # todo: dump to yaml
     label_dict = {
     "1.7. Инвестиции в основной капитал":  ['I','bln_rub'],
     "1.14. Объем платных услуг населению": ['Usl','bln_rub']
@@ -237,31 +337,14 @@ if __name__ == "__main__":
      "в % к соответствующему периоду предыдущего года": 'yoy',
      "в % к предыдущему периоду": 'rog'
      }
-     
-    gen = labelled_row_iter(c2, label_dict, sec_label_dict)
-    dump_iter_to_csv(gen, t2)
-    push_to_database(t2)
-    
 
-                
- 
-
-
-
-
-
-
-           
-            
-
+    t = change_extension(с,"txt")
+    make_labelled_csv(c, t, label_dict, sec_label_dict)
+    print ("Finished writing:", t)
+    
+    push_to_database(t)
+    print ("Pushed to database:", t)
     
     
     
-    
-    
-    
-    
-    
-    
-
     
