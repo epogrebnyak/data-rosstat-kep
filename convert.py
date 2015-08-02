16b9215e17bc23d6b4230963b51634b178cbb469
=epogrebnyak <e.pogrebnyak@gmail.com>
2015-08-02 14:52:54 +0300

    update

--- convert.py
+++ convert.py
@@ -8,6 +8,6 @@ Created on Sun Aug  2 13:22:13 2015
 from word import make_reabable_csv_and_headers as preconvert
 import os
 
-for fn in ["data/1-07/1-07.doc", "ind06/tab.doc", "minitab/minitab.doc"] 
+for fn in ["data/1-07/1-07.doc", "ind06/tab.doc", "minitab/minitab.doc"]:
     p = os.path.abspath(fn)
     s = preconvert(p)
