357bd656d7fe2b46e5a5f1585c90b7d4ea4de7ee
=epogrebnyak <e.pogrebnyak@gmail.com>
2015-08-05 11:28:17 +0300

    can read varnames in order for issue #6

--- src/validate_yaml.py
+++ src/validate_yaml.py
@@ -4,10 +4,41 @@ Created on Wed Aug  5 02:15:13 2015
 
 @author: Евгений
 """
-import yaml as ya
-import os
 
-with open(os.path.abspath("../data/ind06/all_tab_spec.txt")) as d:
-    z3 = ya.load_all(d)
-    for z in z3:
-        print (z) 
+import os
+import yaml
+from collections import OrderedDict
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load_all(stream, OrderedLoader)
+
+# usage example:
+#ordered_load(stream, yaml.SafeLoader)
+
+def load_as_ordered_dict(f):
+    with open(f) as stream:
+        z = [x for x in ordered_load(stream, Loader = yaml.loader.Loader)]
+    return z[0], z[1], z[2]
+
+def load_as_dict(f):        
+    with open(f) as stream:
+        z = [x for x in yaml.load_all(stream)]
+    return z[0], z[1], z[2]  
+
+f = os.path.abspath("../data/ind06/all_tab_spec.txt")
+reader_dict, unit_dict, var_dict = load_as_ordered_dict(f)
+
+# through var_dict to new table in db
+# create  table with auto enumumeration
+
+
+for k, v in var_dict.items():
+    print(k)
