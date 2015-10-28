8e6752178090cf786a2308dd9fc3e2a236af7f28
E Pogrebnyak <e.pogrebnyak@gmail.com>
2015-10-28 04:31:35 +0300

    edits of additional.py

--- src/new_modules/additional.py
+++ src/new_modules/additional.py
@@ -1,9 +1,81 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 27 00:40:31 2015
+"""Compact representation of raw csv import and transformation controlled by yaml config files"""
 
-@author: EP
+###############################################################################
+# 1. INITIAL_DATA
+doc = """line 1
+line 2 something
+line 3
+line 4 more of something
+line 5
+line 101
+line 102
+line 103"""
+
+# 2.1. MAIN CONFIGURATION FILE
+# Proxy for configuration file content
+DOC_segment_info_yaml_filename = """spec0.txt
+------
+line 2 so
+4 more
+spec1.txt
+------
+line 101
+line 103
+spec2.txt
+------"""
+# Result of configuration file import
+some_part_of_starter_line1 = "line 2 so"
+some_part_of_finish_line1 =  "4 more"
+DEFAULT_SPEC_FILE = "spec0.txt"
+SEGMENTS = [[some_part_of_starter_line1, some_part_of_finish_line1, "spec1.txt"],
+            ["line 101",                 "line 103",                "spec2.txt"]]
+# 2.2. SEGMENT CONFIGURATION FILES
+# spec0.txt
+spec0_txt = """
 """
+
+# spec1.txt
+spec1_txt = """
+"""
+# 3. OUTPUT RESULTS
+    
+labelled_rows = [['VARNAME1', 'unit1', 101, 100.5, 100.2, 98.5],
+                 ['VARNAME2', 'unit2',  50,    55,  45.3, 25.7]]
+        
+outside_lines = ['line 1', 'line 4 more of something', 'line 5', '...', 'line 103']
+
+###############################################################################
+
+def get_labelled_rows (raw_csv_filename, segment_info_yaml_filename):
+    raw_rows = read_raw_csv(raw_csv_filename)
+    segment_specs = get_segment_specs(segment_info_yaml_filename)
+    default_spec  = get_default_spec(segment_info_yaml_filename)
+    labelled_rows = label_raw_raws_by_spec(raw_rows, default_spec, segment_specs)    
+    return labelled_rows 
+    
+def read_raw_csv(raw_csv_filename):
+    pass    
+    
+# -----------------------------------------------
+# Issue:...
+def get_segment_spec(yaml_filename):
+    # todo - add import from yaml file + make test function for this import
+    return SEGMENTS 
+    
+def get_default_spec(yaml_filename):
+    # todo - add import from yaml file + make test function for this import
+    pass # DEFAULT_SPEC_FILE    
+# -----------------------------------------------
+
+
+
+
+
+
+
+def label_raw_raws_by_spec(raw_rows, default_spec, segment_specs)
+
 
 def stream_doc(doc):
     for y in doc.split("\n"):      
@@ -20,18 +92,6 @@ def emit_segment(stream, starter_text, end_text):
         if must_emit:
             yield current_row
 
-doc = """line 1
-line 2 something
-line 3
-line 4 more of something
-line 5
-...
-line 101
-line 102
-line 103"""
-
-some_part_of_starter_line1 = "line 2 so"
-some_part_of_finish_line1 =  "4 more"
 
 gen = stream_doc(doc)
 seg = emit_segment(gen, some_part_of_starter_line1, some_part_of_finish_line1)
@@ -49,16 +109,6 @@ def emit_outside_segments(gen, segments):
         for g in r:
             as_list.remove(g)
     return emit_list(as_list)
-     
-S = [[None,                       None,                      "spec0.txt"], 
-     [some_part_of_starter_line1, some_part_of_finish_line1, "spec1.txt"],
-     ["line 101",                 "line 103",                "spec2.txt"]]
-    
-    
-segs = [[s[0], s[1]] for s in S[1:]]
-gen = stream_doc(doc)
-out = emit_outside_segments(gen, segs)
-outside_lines = ['line 1', 'line 4 more of something', 'line 5', '...', 'line 103']
 for ol in outside_lines:
     assert next(out) == ol
 
@@ -90,3 +140,6 @@ def _label_segment(raw_stream, list_of_boundary_lines, list_of_specification_dic
     return yield_row_with_labels(gen_in, headline_dict, support_dict)
 
                  
+segs = [[s[0], s[1]] for s in S[1:]]
+gen = stream_doc(doc)
+out = emit_outside_segments(gen, segs)
