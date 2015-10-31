f9d9baa99f8e79d9b651095ac1a636aeb507eabc
Pastafarianist <mr.pastafarianist@gmail.com>
2015-11-01 02:06:14 +0300

    Implemented spec switching

--- src/new_modules/additional.py
+++ src/new_modules/additional.py
@@ -3,9 +3,11 @@
 
 from common import yield_csv_rows
 from load_spec import load_spec
+from label_csv import is_year, adjust_labels, UNKNOWN_LABELS
 
 ###############################################################################
 from hardcoded import init_raw_csv_file, init_main_yaml, init_config_yaml, init_additional_yaml
+from hardcoded import PARSED_RAW_FILE_AS_LIST
 RAW_FILE = init_raw_csv_file()        
 SPEC_FILE = init_main_yaml()
 
@@ -13,11 +15,11 @@ ADDITIONAL_SPEC_FILE = init_additional_yaml()
 CFG_FILE = init_config_yaml()
 
 
-def get_labelled_rows (raw_csv_filename, segment_info_yaml_filename):
+def get_labelled_rows(raw_csv_filename, segment_info_yaml_filename):
     raw_rows = read_raw_csv_as_list(raw_csv_filename)    
     default_spec  = get_default_spec(segment_info_yaml_filename)
     segment_specs = get_segment_specs(segment_info_yaml_filename)
-    labelled_rows = label_raw_raws_by_spec(raw_rows, default_spec, segment_specs)    
+    labelled_rows = label_raw_rows_by_spec(raw_rows, default_spec, segment_specs)
     return labelled_rows 
 
 def read_raw_csv_as_list(raw_csv_filename):
@@ -49,9 +51,9 @@ assert REF_UNIT_DICT == default_spec[1]
 
 def get_segment_specs(segment_info_yaml_filename):
     yaml = _get_safe_yaml(segment_info_yaml_filename)
-    return [[start_line, end_line, load_spec(specfile)] \
-        for start_line, end_line, specfile in yaml[1:]]
-        
+    return [[start_line, end_line, load_spec(specfile)]
+            for start_line, end_line, specfile in yaml[1:]]
+
 segment_specs = get_segment_specs(CFG_FILE)
 
 expected_segment_specs = [# список
@@ -73,9 +75,48 @@ assert segment_specs == expected_segment_specs
 
 #--------------------
 
-def label_raw_raws_by_spec(raw_rows, default_spec, segment_specs)    :
-    # todo 1 - собрать эту функцию из функций Load_csv
-    pass  
 
-# todo 2 - сделать assert по поводу результатов label_raw_raws_by_spec
-# по идее должно выдавать PARSED_RAW_FILE_AS_LIST из hardocoded.py но может в другом порядке рядов
+def label_raw_rows_by_spec(raw_rows, default_spec, segment_specs):
+    labels = UNKNOWN_LABELS[:]
+
+    in_segment = False
+    current_spec = default_spec
+    current_end_line = None
+
+    labelled_rows = []
+
+    for row in raw_rows:
+        if not row[0]:
+            # junk row
+            continue
+
+        # Are we in the default spec?
+        if not in_segment:
+            # Do we have to switch to a custom one?
+            for start_line, end_line, spec in segment_specs:
+                if row[0].startswith(start_line):
+                    # Yes!
+                    in_segment = True
+                    current_spec = spec
+                    current_end_line = end_line
+                    break
+        else:
+            # We are in a custom spec. Do we have to switch to the default one?
+            if row[0].startswith(current_end_line):
+                in_segment = False
+                current_spec = default_spec
+                current_end_line = None
+
+        # Spec has been possibly switched, now may have to adjust labels
+        if not is_year(row[0]):
+            # label-switching row
+            labels = adjust_labels(row[0], labels, current_spec[0], current_spec[1])
+        else:
+            # data row
+            labelled_rows.append(labels + row)
+
+    return labelled_rows
+
+raw_csv = read_raw_csv_as_list(RAW_FILE)
+labelled_rows = label_raw_rows_by_spec(raw_csv, default_spec, segment_specs)
+assert labelled_rows == PARSED_RAW_FILE_AS_LIST
