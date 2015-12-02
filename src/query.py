6760e157c6947d9c16f72df836bf0de595785d78
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-12-02 12:08:39 +0300

    Update query.py

--- src/query.py
+++ src/query.py
@@ -66,75 +66,9 @@ def reshape_m(dfm):
     else: 
         return pd.DataFrame()
 
-#-----------------------------------------------------------------------------------
-
-
-def write_to_xl(dfa, dfq, dfm):
-   # Not run/not tested. For issue #28
-   df_var_names = get_var_list_as_dataframe()
-   for file in [XLSX_FILE, XLS_FILE]:
-      _write_to_xl(dfa, dfq, dfm, df_var_names, file)
-
-def _write_to_xl(dfa, dfq, dfm, df_var_names, file):
-    with pd.ExcelWriter(file) as writer:
-        dfa.to_excel(writer, sheet_name='year')
-        dfq.to_excel(writer, sheet_name='quarter')
-        dfm.to_excel(writer, sheet_name='month')
-        df_var_names.to_excel(writer, sheet_name='variables')
-    # copy file to root directory     
-    shutil.copy(file, "..")
-
-# ---------------------------------------------------------------------------------
-# Legacy code, for deletion
-
-def get_additional_header(df):
-    return ["date"] + df.columns.values.tolist()
-    
-def get_csvrows(df):
-    strings = df.to_csv(sep = "\t", decimal = ",", header = False)
-    # note: below will not be needed in pandas 0.16
-    #       undesired - will change . for , in headers too
-    strings = strings.replace(".", ",")
-    return [x.split("\t") for x in strings.split("\n")]
-
-def df_csv_iter(df):
-    # MUST ADD: restore original order of items as in spec dictionary + rebase df
-    yield get_additional_header(df) 
-    for row in get_csvrows(df):
-        yield row
-        
-# End of deletion 
-# ---------------------------------------------------------------------------------
-
-def to_csv(df, filename):
-   # Previous version: 
-   # dump_iter_to_csv(df_csv_iter(df), filename)
-   #
-   # We simplify it with following:
-   df.to_csv(filename)
-   # Reference call:
-   # DataFrame.to_csv(path_or_buf=None, sep=', ', na_rep='', float_format=None, columns=None, header=True, index=True, index_label=None, mode='w', encoding=None, quoting=None, quotechar='"', line_terminator='\n', chunksize=None, tupleize_cols=False, date_format=None, doublequote=True, escapechar=None, decimal='.', **kwds)
-
-def write_to_csv(dfa, dfq, dfm):
-    to_csv(dfa, ANNUAL_CSV)
-    to_csv(dfq, QUARTERLY_CSV)
-    to_csv(dfm, MONTHLY_CSV)   
-
-def get_reshaped_dfs():
-    dfa, dfq, dfm = read_dfs()
-    check_for_dups(dfa)
-    dfa = reshape_a(dfa)
-    dfq = reshape_q(dfq)
-    dfm = reshape_m(dfm)
-    return dfa, dfq, dfm
-
-def db_dump():
-    dfa, dfq, dfm = get_reshaped_dfs()
-    write_to_xl(dfa, dfq, dfm)
-    write_to_csv(dfa, dfq, dfm)
-
 #------------------------------------------------------------------------------
-# Functions to obtain dfm, dfa, dfq
+# Functions to slice dfm, dfa, dfq
+# These are end-user API functions
 
 def date_to_tuple(input_date):
     if isinstance(input_date, int):
@@ -206,18 +140,98 @@ def get_dfm():
     var_names = get_var_list()
     return get_DataFrame(var_names, "m", "1999-01")
 
+# ----------------------------------------------------------------------
+
 def test_get_df_and_ts():
-    z = get_time_series('WAGE_rub','a', 2014)
+    z = get_ts('WAGE_rub','a', 2014)
     assert isinstance(z, pd.core.series.Series)
     assert z.iloc[0] == 32495
 
-    e = get_dataframe(['WAGE_rub', 'CPI_rog'], 'm', '2015-06', '2015-06')
+    e = get_df(['WAGE_rub', 'CPI_rog'], 'm', '2015-06', '2015-06')
     assert isinstance(e, pd.DataFrame)
     # WARNING: this is data revision - in ind06 this was 
     # assert e.iloc[0,0] == 35930.0
     # now in ind09 it is:
     assert e.iloc[0,0] == 35395
     assert e.iloc[0,1] == 100.2
+
+#-----------------------------------------------------------------------------------
+
+def get_var_list():
+    dfa, dfq, dfm = read_dfs()
+    dfa = reshape_a(dfa)
+    return dfa.columns.values.tolist()  
+    
+#-----------------------------------------------------------------------------------
+# Excel output
+
+def write_to_xl(dfa, dfq, dfm):
+   # Not run/not tested. For issue #28
+   df_var_names = get_var_list_as_dataframe()
+   for file in [XLSX_FILE, XLS_FILE]:
+      _write_to_xl(dfa, dfq, dfm, df_var_names, file)
+
+def _write_to_xl(dfa, dfq, dfm, df_var_names, file):
+    with pd.ExcelWriter(file) as writer:
+        dfa.to_excel(writer, sheet_name='year')
+        dfq.to_excel(writer, sheet_name='quarter')
+        dfm.to_excel(writer, sheet_name='month')
+        df_var_names.to_excel(writer, sheet_name='variables')
+    # copy file to root directory     
+    shutil.copy(file, "..")
+
+# ---------------------------------------------------------------------------------
+# CSV output
+
+# ---------------------------------------------------------------------------------
+# Legacy code, for deletion
+
+def get_additional_header(df):
+    return ["date"] + df.columns.values.tolist()
+    
+def get_csvrows(df):
+    strings = df.to_csv(sep = "\t", decimal = ",", header = False)
+    # note: below will not be needed in pandas 0.16
+    #       undesired - will change . for , in headers too
+    strings = strings.replace(".", ",")
+    return [x.split("\t") for x in strings.split("\n")]
+
+def df_csv_iter(df):
+    # MUST ADD: restore original order of items as in spec dictionary + rebase df
+    yield get_additional_header(df) 
+    for row in get_csvrows(df):
+        yield row
+        
+# End of deletion 
+# ---------------------------------------------------------------------------------
+
+
+def to_csv(df, filename):
+   # Previous version: 
+   # dump_iter_to_csv(df_csv_iter(df), filename)
+
+   # We simplify it with following:
+   df.to_csv(filename)
+   # Reference call:
+   # DataFrame.to_csv(path_or_buf=None, sep=', ', na_rep='', float_format=None, columns=None, header=True, index=True, index_label=None, mode='w', encoding=None, quoting=None, quotechar='"', line_terminator='\n', chunksize=None, tupleize_cols=False, date_format=None, doublequote=True, escapechar=None, decimal='.', **kwds)
+
+def write_to_csv(dfa, dfq, dfm):
+    to_csv(dfa, ANNUAL_CSV)
+    to_csv(dfq, QUARTERLY_CSV)
+    to_csv(dfm, MONTHLY_CSV)   
+
+def get_reshaped_dfs():
+    dfa, dfq, dfm = read_dfs()
+    check_for_dups(dfa)
+    dfa = reshape_a(dfa)
+    dfq = reshape_q(dfq)
+    dfm = reshape_m(dfm)
+    return dfa, dfq, dfm
+
+def db_dump():
+    dfa, dfq, dfm = get_reshaped_dfs()
+    write_to_xl(dfa, dfq, dfm)
+    write_to_csv(dfa, dfq, dfm)
 
 if __name__ == "__main__":
     dfa, dfq, dfm = read_dfs()
@@ -226,10 +240,8 @@ if __name__ == "__main__":
     write_to_xl(dfa, dfq, dfm)
 
     #test_get_df_and_ts()
-
- 
-#TODO:
-# may change formatting of the columns http://xlsxwriter.readthedocs.org/en/latest/example_pandas_column_formats.html#ex-pandas-column-formats
-# http://stackoverflow.com/questions/17069694/writing-xlwt-dates-with-excel-date-format
-# http://stackoverflow.com/questions/9920935/easily-write-formatted-excel-from-python-start-with-excel-formatted-use-it-in
-# do not write second row - inherited from pivot.   
+    
+# NOTE: file contains both output functions dump_db() and query functions get_ts(), get_df()
+#       also in this file: 
+#       - get_dfm()
+#       - from var_names import get_var_list_as_dataframe may be a circular reference with var_names.py as it uses get_var_list()
