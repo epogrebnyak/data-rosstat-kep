61345dcb001fd97190a9105224264a78282713ae
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-12-22 15:28:43 +0300

    Update test_database.py

--- kep/test/test_database.py
+++ kep/test/test_database.py
@@ -2,8 +2,9 @@ from kep.importer.parser.stream import stream_flat_data
 from kep.query.save import get_reshaped_dfs
 from kep.database.db import stream_to_database
 
+# using fixtures: yes (labelled_rows)
+# comment: very weak test, possibly to be revised
 def test_database(labelled_rows):
-    # where is labelled_rows coming from? 
     gen = list(stream_flat_data(labelled_rows))
     stream_to_database(gen)
     dfa, dfq, dfm = get_reshaped_dfs()
