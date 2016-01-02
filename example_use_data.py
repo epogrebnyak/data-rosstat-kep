a98b3e0f49db74908de8b8b288e2c569b51f8871
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-01-03 00:50:10 +0300

    API draft - #82

--- example_use_data.py
+++ example_use_data.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
 ### Получение данных
 
-from kep import get_ts, get_df, get_varnames, get_all_dfs 
+from kep import get_ts, get_df
+from kep import get_dfa, get_dfq, get_dfm, get_all_dfs 
+from kep import get_varnames 
 
-# query by variable name(s) 
-z = get_ts('SOC_WAGE_rub','a', 2014)
-assert z.loc[2014] == 32495
+# query by variable name - obtain pandas time series
+ts1 = get_ts('SOC_WAGE_rub','a', 1999)
+assert ts1.loc[2014] == 32495
 
-e = get_df(['SOC_WAGE_rub', 'CPI_rog'], 'm', '2015-06', '2015-06')
+# query by variable names - obtain pandas dataframe
+df1 = get_df(['SOC_WAGE_rub', 'CPI_rog'], 'm', '2010-01', '2015-10')
+# TODO: assert
 
-# complete database:
-annual_varnames = get_varnames("a")
-qtr_varnames    = get_varnames("q")
-month_varnames  = get_varnames("m")
-all_varnames    = get_varnames()
-
-dfa = get_df(annual_varnames, "a")
-dfq = get_df(qtr_varnames, "q")
-dfm = get_df(month_varnames, "m")
-
-# TODO: maybe:
-# import ...
-# dfa = get_dfa()
-# dfq = get_dfq()
-# dfm = get_dfm()
+# get all 
+dfa = get_dfa()
+dfq = get_dfq()
+dfm = get_dfm()
 
 # or simply: 
 dfa, dfq, dfm = get_all_dfs()
