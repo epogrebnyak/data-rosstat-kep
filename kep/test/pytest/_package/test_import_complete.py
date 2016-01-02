042219df2daaee1142fcd4b48ad5d36628a9fcc3
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-01-03 00:30:32 +0300

    test updated

--- kep/test/pytest/_package/test_import_complete.py
+++ kep/test/pytest/_package/test_import_complete.py
@@ -8,7 +8,6 @@ def test_get_complete_dicts(data_folder = CURRENT_MONTH_DATA_FOLDER):
     assert 'CPI_FOOD' in [z[0] for z in hdr.values()]    
 
 def test_import_complete(data_folder = CURRENT_MONTH_DATA_FOLDER):
-    # TODO: add config file to import 'PROFIT' 
     import_csv(data_folder, kill_existing_data = True)
-    assert len(get_varnames_not_in_db(data_folder)) == 1
-    assert get_varnames_not_in_db(data_folder) == ["PROFIT"]
+    assert len(get_varnames_not_in_db(data_folder)) == 0
+    # assert get_varnames_not_in_db(data_folder) == ["PROFIT"]
