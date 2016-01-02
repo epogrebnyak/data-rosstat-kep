dc73cac95af5bad63495c88eb88b53d42702d894
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-02 22:58:07 +0300

    example_export.py - 6 steps sharp - addresses #85

--- example_import.py
+++ example_import.py
@@ -5,25 +5,22 @@ Note: same as kep.update() or kep\\update.py"""
 import kep 
 data_folder = "data/2015/ind10"
 
-# convert Word files to csv 
+# 1. convert Word files to csv 
 kep.make_csv(data_folder)
 
-# parse and import csv to database
+# 2. parse and import csv to database
 kep.import_csv(data_folder)
 
-# save data and variable list to Excel and CSV files 
-kep.db_dump()
-
-# run some inspection 
+# 3. run some inspection 
 kep.notify_on_import_result(data_folder)    
     
-# export times series from database to CSV files and Excel xls(x)
+# 4. export times series from database to CSV and Excel xls(x)
 kep.db_dump()
     
-# create and save PDF and *.png graphs
+# 5. create and save PDF and *.png graphs
 kep.write_plots()
     
-# keprites list of variables to nmarkdown file 
+# 6. keprites list of variables to markdown file 
 kep.dump_var_list_explained()
 
 # NOTE:
