7fcecae28ea30e46726c7b41b556196dd1ecfc42
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-01-04 07:04:49 +0300

    test cleanup

--- kep/test/pytest/remaining/_package/test_example_use_data.py
+++ kep/test/pytest/remaining/_package/test_example_use_data.py
@@ -4,14 +4,13 @@ from kep.query.end_user import get_ts, get_df
 from kep.importer.csv2db import import_csv
 from kep.paths import CURRENT_MONTH_DATA_FOLDER
 
-# fixture: implicit - import_csv(CURRENT_MONTH_DATA_FOLDER), may be a function at start of module + split test to two functions
 def test_get_df_and_ts():
     import_csv(CURRENT_MONTH_DATA_FOLDER)
     z = get_ts('SOC_WAGE_rub','a', 2014)
     assert isinstance(z, pd.core.series.Series)
     assert z.iloc[0] == 32495
 
-    e = get_df(['SOC_WAGE_rub', 'CPI_rog'], 'm', '2015-06', '2015-06')
+    e = get_df(['SOC_WAGE_rub', 'CPI_rog'], 'm', '2015-06', '2015-10')
     assert isinstance(e, pd.DataFrame)
     # WARNING: this is data revision - in ind06 this was 
     # assert e.iloc[0,0] == 35930.0
@@ -19,5 +18,3 @@ def test_get_df_and_ts():
     assert e.iloc[0,0] == 35395
     assert e.iloc[0,1] == 100.2
 
-if __name__ == "__main__":
-    test_get_df_and_ts()
