15110f84675d2e910f3a3e65f5dc86b1e5af44d1
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-12 18:04:44 +0300

    Update test_definitions.py

--- kep/rowsystem/class/test_definitions.py
+++ kep/rowsystem/class/test_definitions.py
@@ -53,4 +53,29 @@ def test_YAML():
 def test_CSV():        
     test_list = ["123","456"] # NOTE: fails when \n inside list 
     test_string = "\n".join(test_list)
-    assert test_list == CSV(test_string).rows   
+    assert test_list == CSV(test_string).rows 
+
+# ----------------------------------------------------------------------------------
+# Validate config files TODO`     
+def untest_cfg_import():
+    write_spec_files()
+    cfg = write_cfg()    
+    assert SegmentList(cfg).segments == cfg_list    
+    
+def cmp_spec(doc,var):
+    cnt = Segment(doc)._as_load_spec 
+    #import pdb; pdb.set_trace()
+    assert var == cnt
+            
+def test_spec():
+    cmp_spec(doc=spec_ip_doc, var=(header_dicts['ip'], unit_dicts['ip'], null_segment_dict))
+    
+    cmp_spec(doc=spec_ip_trans_inv_doc, 
+             var=(join_header_dicts(['ip','trans','investment']),
+                  common_unit_dict, null_segment_dict))
+                  
+    cmp_spec(doc=spec_cpi_block, 
+             var=(header_dicts['cpi_block'], unit_dicts['cpi_block'], cpi_segment_dict))
+
+    cmp_spec(doc=spec_food_block, 
+             var=(header_dicts['food_block'], unit_dicts['food_block'], food_segment_dict))
