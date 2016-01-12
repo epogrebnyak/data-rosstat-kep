12f617b3f59bbb893c410711e95886d0cf4745c5
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-12 13:15:54 +0300

    Create _example.py

---
+++ kep/rowsystem/class/_example.py
@@ -0,0 +1,21 @@
+# MAYDO: write fixture here with some importable files
+# TODO: add new specification to current month folder 
+
+import os 
+from rowsystem import RowSystem
+from kep_db import KEP
+from path_config import CURRENT_MONTH_DATA_FOLDER, current_folder
+
+folder =  CURRENT_MONTH_DATA_FOLDER
+
+rs = RowSystem(folder)
+    # reads input definition from standard files
+    # labels rows
+    # starts pandas interface
+
+rs.save()
+    # saves data to default database or freeze files 
+
+kep = KEP()
+dfa = kep.get_annual()
+print(dfa.to_csv())
