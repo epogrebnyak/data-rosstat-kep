1c126876cf58e4573346fc45b4527f0e1df1b7a7
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-12 13:12:59 +0300

    Create path_config.py

---
+++ kep/rowsystem/class/path_config.py
@@ -0,0 +1,11 @@
+def current_folder():
+    curpath = os.path.realpath(__file__)
+    return level_up(curpath)
+
+def level_up(path, n = 1):
+    for i in range(n):
+        path = os.path.split(path)[0]
+    return path
+
+REPO_ROOT_FOLDER = level_up(current_folder(), n = 3)
+CURRENT_MONTH_DATA_FOLDER = os.path.join(REPO_ROOT_FOLDER, 'data', '2015', 'ind11')
