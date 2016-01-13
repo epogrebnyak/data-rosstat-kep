db81acb22b036a5b0f69d80f21eab0a212aa72c7
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-13 17:11:37 +0300

    Update path_config.py

--- kep/rowsystem/class/path_config.py
+++ kep/rowsystem/class/path_config.py
@@ -12,4 +12,20 @@ def level_up(path, n = 1):
     return path
 
 REPO_ROOT_FOLDER = level_up(current_folder(), n = 3)
+
+# data
 CURRENT_MONTH_DATA_FOLDER = os.path.join(REPO_ROOT_FOLDER, 'data', '2015', 'ind11')
+
+# graphic output
+OUTPUT_DIR    = os.path.join(PROJECT_ROOT_PATH, 'output')
+PDF_FILE      = os.path.join(OUTPUT_DIR, 'monthly.pdf')
+MD_FILE       = os.path.join(OUTPUT_DIR, 'images.md')
+PNG_FOLDER    = os.path.join(OUTPUT_DIR, 'png')
+VARNAMES_FILE = os.path.join(OUTPUT_DIR, 'varnames.md')
+
+# xls and csv output
+XLSX_FILE   = os.path.join(OUTPUT_DIR, 'kep.xlsx')
+XLS_FILE    = os.path.join(OUTPUT_DIR, 'kep.xls')
+ANNUAL_CSV  = os.path.join(OUTPUT_DIR, 'data_annual.txt')
+QUARTER_CSV = os.path.join(OUTPUT_DIR, 'data_quarter.txt')
+MONTHLY_CSV = os.path.join(OUTPUT_DIR, 'data_monthly.txt')
