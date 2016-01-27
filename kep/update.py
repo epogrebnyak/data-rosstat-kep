c3d20a4697dd6ad1737cefb38739833c79dc46f4
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-27 10:56:48 +0300

    Update update.py

--- kep/update.py
+++ kep/update.py
@@ -1,15 +1,6 @@
-from kep.admin import Admin
 from publish import Publisher
 from rs import CurrentMonthRowSystem
    
 if __name__ == "__main__":
-    Admin().update() 
-    
-    m = CurrentMonthRowSystem()
-    m.save()
-    m.toc(to_file = True)
-    
-    #Publisher().write_varnames_markdown()    
-    
-    
-    #Publisher().publish()
+    CurrentMonthRowSystem().update()
+    Publisher().publish()
