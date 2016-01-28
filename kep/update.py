4fc3483bfcf186b953c2cc4b8d2804daa7aad25a
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-28 17:18:39 +0300

    Update update.py

--- kep/update.py
+++ kep/update.py
@@ -1,6 +1,8 @@
-from kep.extract.publish import Publisher
 from kep.reader.rs import CurrentMonthRowSystem
+from kep.extract.publish import Publisher
+from kep.extract.dataframes import KEP
    
 if __name__ == "__main__":
     CurrentMonthRowSystem().update()
     Publisher().publish()
+    kep = KEP()
