2d76e9e218bcdb0848367716eb787deda131d4ff
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-01-28 17:20:03 +0300

    Delete update.py

--- kep/update.py
+++
@@ -1,8 +0,0 @@
-from kep.reader.rs import CurrentMonthRowSystem
-from kep.extract.publish import Publisher
-from kep.extract.dataframes import KEP
-   
-if __name__ == "__main__":
-    CurrentMonthRowSystem().update()
-    Publisher().publish()
-    kep = KEP()
