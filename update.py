4ff7c6f4326678d7493b37cae32606fc5b14fa48
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-04-05 19:56:15 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,13 @@
-from kep import KEP
+from kep import KEP, parse_doc_files
 
+# convert most recent doc files to csv
+parse_doc_files()
+
+# parse csv
 k = KEP().update()
+# obtain dataframes
 dfa, dfq, dfm = k.get_all()
+# dump outputs
 k = k.write_xl()
 k.write_monthly_pdf()
 k.write_monthly_png()
