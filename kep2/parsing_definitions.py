bf886c0b0d3127c45f880153a69e98a26e1bfad8
Dmitry Platonov <dmitry.platonov@gmail.com>
2017-04-13 00:14:28 +0300

    Switch from dict to OrderedDict as representaion of parsing rules

--- kep2/parsing_definitions.py
+++ kep2/parsing_definitions.py
@@ -1,17 +1,35 @@
 """Read parsing specification from yaml file ('specfile')."""
 
-import yaml
-import tempfile
+from collections import OrderedDict
 import os
-from csv_data import File
+import tempfile
+import yaml
 
 from config import get_default_spec_path, get_all_spec_paths
+from csv_data import File
 
 
 def get_definitions():
     return {'default': ParsingDefinition(path=get_default_spec_path()),
             'additional': [ParsingDefinition(path) for path in get_all_spec_paths()]
             }
+
+
+# Make yaml load dicts as OrderedDicts - start
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+
+def _dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+
+
+def _dict_constructor(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+
+
+yaml.add_representer(OrderedDict, _dict_representer)
+yaml.add_constructor(_mapping_tag, _dict_constructor)
+# Make yaml load dicts as OrderedDicts - end
 
 
 class ParsingDefinition():
@@ -60,7 +78,7 @@ special reader: fiscal
     def __init__(self, path: str):
         """Read data from *path* file"""
         yaml_string = File(path).read_text()
-        self.content = self.load_yaml_string(yaml_string)
+        self.content = self.from_yaml(yaml_string)
         # FIXME
         # self.check()
         self.attrs = {'start': self.content[0]['start line'],
@@ -73,8 +91,11 @@ special reader: fiscal
                       }
 
     @staticmethod
-    def load_yaml_string(yaml_string):
-        return list(yaml.load_all(yaml_string))
+    def from_yaml(yaml_string):
+        definitions = list(yaml.load_all(yaml_string))
+        # Replace every occurence of None with empty OrderedDict
+        definitions = [x if x is not None else OrderedDict() for x in definitions]
+        return definitions
 
     @staticmethod
     def check_parsed_yaml(content):
@@ -126,27 +147,31 @@ if __name__ == "__main__":
 
     # testing
     CONTENT_1 = [
-        {'start line': None, 'end line': None, 'special reader': None},
-        {'в % к предыдущему периоду': 'rog',
-         'период с начала отчетного года в % к соответствующему периоду предыдущего года': 'ytd',
-         'в % к соответствующему периоду предыдущего года': 'yoy'},
-        {'Объем ВВП': ['GDP', 'bln_rub'],
-         'Индекс физического объема произведенного ВВП': ['GDP', 'rog'],
-         'Индекс промышленного производства': ['IND_PROD']}]
+        OrderedDict([('start line', None), ('end line', None), ('special reader', None)]),
+        OrderedDict([('в % к предыдущему периоду', 'rog'),
+            ('период с начала отчетного года в % к соответствующему периоду предыдущего года', 'ytd'),
+            ('в % к соответствующему периоду предыдущего года', 'yoy'),
+        ]),
+        OrderedDict([('Объем ВВП', ['GDP', 'bln_rub']),
+            ('Индекс физического объема произведенного ВВП', ['GDP', 'rog']),
+            ('Индекс промышленного производства', ['IND_PROD']),
+        ]),
+    ]
 
     CONTENT_2 = [
-        {'start line': '2.1.1. Доходы (по данным Федерального казначейства)',
-         'end line': 'Удельный вес в общем объеме доходов соответствующего бюджета',
-         'special reader': 'fiscal'},
-        # ERROR must convert none to empty dict
-        None,
-        {'Консолидированный бюджет': ['GOV_CONSOLIDATED_REVENUE_ACCUM', 'bln_rub']}]
+        OrderedDict([('start line', '2.1.1. Доходы (по данным Федерального казначейства)'),
+            ('end line', 'Удельный вес в общем объеме доходов соответствующего бюджета'),
+            ('special reader', 'fiscal'),
+        ]),
+        OrderedDict(),
+        OrderedDict([('Консолидированный бюджет', ['GOV_CONSOLIDATED_REVENUE_ACCUM', 'bln_rub'])]),
+    ]
 
     test_data = [(ParsingDefinition.YAML_SAMPLE_1, CONTENT_1),
                  (ParsingDefinition.YAML_SAMPLE_2, CONTENT_2)]
 
     for yaml_string, loaded_content in test_data:
-        assert loaded_content == ParsingDefinition.load_yaml_string(yaml_string)
+        assert loaded_content == ParsingDefinition.from_yaml(yaml_string)
 
     with Tempfile(yaml_string=ParsingDefinition.YAML_SAMPLE_1) as path:
         assert ParsingDefinition(path).content == CONTENT_1
