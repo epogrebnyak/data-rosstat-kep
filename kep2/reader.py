55cb1a626940c4c1949c33e83394643bc19f5e2c
Dmitry Platonov <dmitry.platonov@gmail.com>
2017-04-13 00:48:42 +0300

    Testing via unittest; test cases added

--- kep2/reader.py
+++ kep2/reader.py
@@ -1,7 +1,9 @@
+import unittest 
+
 from config import get_default_spec_path, get_default_csv_path
-from parsing_definitions import ParsingDefinition
 from csv_data import CSV_Reader
 from datapoints import Datapoints
+from parsing_definitions import ParsingDefinition
 
 # data
 csv_path = get_default_csv_path()
@@ -19,22 +21,40 @@ assert d.datapoints[0] == {'freq': 'a', 'value': 4823.0, 'varname': 'GDP_bln_rub
 # truncate csv_dicts, too many errors in whole file
 output = list(d.emit('a'))[:140]
 
-for z in output:
-    if z['year'] == 2016:
-        print(z.__repr__() + ",")
+def show_2016():
+    for z in output:
+        if z['year'] == 2016:
+            print(z.__repr__() + ",")
 
-testpoints = [
+testpoints_valid = [
     {'varname': 'GDP_bln_rub', 'year': 2016, 'value': 85881.0},
     {'varname': 'GDP_rog', 'year': 2016, 'value': 99.8},
     {'varname': 'IND_PROD_yoy', 'year': 2016, 'value': 101.3},
     {'varname': 'AGRO_PRODUCTION_yoy', 'year': 2016, 'value': 104.8},
     {'varname': 'PROD_AGRO_MEAT_th_t', 'year': 2016, 'value': 13939.0},
     {'varname': 'PROD_AGRO_MEAT_yoy', 'year': 2016, 'value': 103.4},
-    # ERROR CRITICAL: - same label, header or unit did not switch, must edit "__spec.txt" 
+    {'varname': 'PROD_AGRO_MILK_th_t', 'year': 2016, 'value': 30724.0},
+    {'varname': 'PROD_AGRO_MILK_yoy', 'year': 2016, 'value': 99.8},
+]
+
+testpoints_invalid = [
+    # ERROR CRITICAL: - same label, header or unit did not switch, must edit "__spec.txt"
     {'varname': 'PROD_AGRO_MEAT_yoy', 'year': 2016, 'value': 30724.0},
     # ERROR CRITICAL: - same label, header or unit did not switch, must edit "__spec.txt"
     {'varname': 'PROD_AGRO_MEAT_yoy', 'year': 2016, 'value': 99.8},
 ]
 
-for t in testpoints:
-    assert t in output
+
+class TestCase2016(unittest.TestCase):
+    def test_positive(self):
+        for t in testpoints_valid:
+            assert t in output
+
+    def test_negative(self):
+        for t in testpoints_invalid:
+            assert t not in output
+
+
+if __name__ == '__main__':
+    #show_2016()
+    unittest.main()
