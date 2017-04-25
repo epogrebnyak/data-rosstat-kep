fa01032a17276496fb7ebba6e0371faa0153a020
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-04-25 10:49:56 +0300

    Update parsing_definitions.py

--- kep2/parsing_definitions.py
+++ kep2/parsing_definitions.py
@@ -2,16 +2,15 @@
 
 Usage:
     
-    pdef = ParsingDefinition(path=get_default_spec_path())
+    ParsingDefinition(path)
     
-    Attributes for *pdef*:
+    Class attributes:
        .start
        .end
-       .splitter_func_name
+       .reader
        .units
        .headers
-       .labels
-       
+       .all_labels       
 """
 
 from collections import OrderedDict
@@ -28,7 +27,7 @@ def get_definitions(folder=config.get_default_spec_folder()):
             'additional': [ParsingDefinition(path) for path in _path_list]
             }
 
-# Make yaml load dicts as OrderedDicts - start
+# Make yaml load dicts as OrderedDicts - start -------------
 _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
 
 
@@ -44,73 +43,6 @@ yaml.add_representer(OrderedDict, _dict_representer)
 yaml.add_constructor(_mapping_tag, _dict_constructor)
 # ----------------------------------------------------- end
 
-# ----------------------------------------------------- constants
-# Keep YAML_SAMPLE and CONTENT constants here for quick reference
-
-#  YAML_1 - parsing instruction with no start and end line and no special reader,
-
-YAML_1 = """
-# scope 
-start line: null       # 'start_line'
-end line: null         # 'end_line'
-special reader: null   # 'reader_func'
----
-# 'units' section 
-в % к предыдущему периоду: rog
-период с начала отчетного года в % к соответствующему периоду предыдущего года: ytd
-в % к соответствующему периоду предыдущего года: yoy
----
-# 'headers' section 
-Объем ВВП: 
- - GDP
- - bln_rub
-Индекс физического объема произведенного ВВП:
- - GDP
- - rog
-Индекс промышленного производства:
- - IND_PROD
-"""
-
-CONTENT_1 = [
-        OrderedDict([('start line', None), ('end line', None), ('special reader', None)]),
-        OrderedDict([('в % к предыдущему периоду', 'rog'),
-            ('период с начала отчетного года в % к соответствующему периоду предыдущего года', 'ytd'),
-            ('в % к соответствующему периоду предыдущего года', 'yoy'),
-        ]),
-        OrderedDict([('Объем ВВП', ['GDP', 'bln_rub']),
-            ('Индекс физического объема произведенного ВВП', ['GDP', 'rog']),
-            ('Индекс промышленного производства', ['IND_PROD']),
-        ]),
-    ]
-
-          
-#  YAML_2 - parsing instruction with start and end line and special reader.
-
-YAML_2 = """
-# scope and reader defined
-start line : 2.1.1. Доходы (по данным Федерального казначейства)
-end line : Удельный вес в общем объеме доходов соответствующего бюджета
-special reader: fiscal
----
-# units skipped
----
-# headers: one variable to be read
-Консолидированный бюджет : 
- - GOV_CONSOLIDATED_REVENUE_ACCUM
- - bln_rub 
-"""
-
-CONTENT_2 = [
-        OrderedDict([('start line', '2.1.1. Доходы (по данным Федерального казначейства)'),
-            ('end line', 'Удельный вес в общем объеме доходов соответствующего бюджета'),
-            ('special reader', 'fiscal'),
-        ]),
-        OrderedDict(),
-        OrderedDict([('Консолидированный бюджет', ['GOV_CONSOLIDATED_REVENUE_ACCUM', 'bln_rub'])]),
-    ]
-
-# ------------------------------------------------------------ end of constants
-
 class StringAsYAML():
     """Read parsing instruction from string. String format is similar to YAML_1."""
     
@@ -125,7 +57,7 @@ class StringAsYAML():
                       'units': self.content[1],
                       # WONTFIX variable names are a list now, but can be a label dict for easier handling later
                       'headers': self.content[2],
-                      'labels': [v[0] for v in self.content[2].values()]
+                      'all_labels': [v[0] for v in self.content[2].values()]
                       }
 
     @staticmethod
