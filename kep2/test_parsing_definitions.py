7abeb7e6cbb13acfe3c5841fc678a9eb88ef29a4
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-04-25 10:52:11 +0300

    Update test_parsing_definitions.py

--- kep2/test_parsing_definitions.py
+++ kep2/test_parsing_definitions.py
@@ -35,6 +35,66 @@ r:
  - sss
  - t"""
 
+#  YAML_1 - parsing instruction with no start and end line and no special reader,
+YAML_1 = """
+# scope 
+start line: null       # 'start_line'
+end line: null         # 'end_line'
+special reader: null   # 'reader_func'
+---
+# 'units' section 
+в % к предыдущему периоду: rog
+период с начала отчетного года в % к соответствующему периоду предыдущего года: ytd
+в % к соответствующему периоду предыдущего года: yoy
+---
+# 'headers' section 
+Объем ВВП: 
+ - GDP
+ - bln_rub
+Индекс физического объема произведенного ВВП:
+ - GDP
+ - rog
+Индекс промышленного производства:
+ - IND_PROD
+"""
+
+CONTENT_1 = [
+        OrderedDict([('start line', None), ('end line', None), ('special reader', None)]),
+        OrderedDict([('в % к предыдущему периоду', 'rog'),
+            ('период с начала отчетного года в % к соответствующему периоду предыдущего года', 'ytd'),
+            ('в % к соответствующему периоду предыдущего года', 'yoy'),
+        ]),
+        OrderedDict([('Объем ВВП', ['GDP', 'bln_rub']),
+            ('Индекс физического объема произведенного ВВП', ['GDP', 'rog']),
+            ('Индекс промышленного производства', ['IND_PROD']),
+        ]),
+    ]
+
+          
+#  YAML_2 - parsing instruction with start and end line and special reader.
+YAML_2 = """
+# scope and reader defined
+start line : 2.1.1. Доходы (по данным Федерального казначейства)
+end line : Удельный вес в общем объеме доходов соответствующего бюджета
+special reader: fiscal
+---
+# units skipped
+---
+# headers: one variable to be read
+Консолидированный бюджет : 
+ - GOV_CONSOLIDATED_REVENUE_ACCUM
+ - bln_rub 
+"""
+
+CONTENT_2 = [
+        OrderedDict([('start line', '2.1.1. Доходы (по данным Федерального казначейства)'),
+            ('end line', 'Удельный вес в общем объеме доходов соответствующего бюджета'),
+            ('special reader', 'fiscal'),
+        ]),
+        OrderedDict(),
+        OrderedDict([('Консолидированный бюджет', ['GOV_CONSOLIDATED_REVENUE_ACCUM', 'bln_rub'])]),
+    ]
+
 
 class Test_StringAsYAML(unittest.TestCase):
         
@@ -44,8 +104,8 @@ class Test_StringAsYAML(unittest.TestCase):
        pdef.StringAsYAML.check_parsed_yaml(yaml_content)
 
     def test_read_yaml_and_check(self):       
-       self.read_and_check(pdef.YAML_1)
-       self.read_and_check(pdef.YAML_2)
+       self.read_and_check(YAML_1)
+       self.read_and_check(YAML_2)
        self.read_and_check(EMPTY)
        self.read_and_check(MINIMAL)
        
@@ -55,8 +115,8 @@ class Test_StringAsYAML(unittest.TestCase):
     
     def test_yaml_string_to_content(self):
        self.string_to_content(EMPTY, EMPTY_CONTENT)
-       self.string_to_content(pdef.YAML_1, pdef.CONTENT_1)
-       self.string_to_content(pdef.YAML_2, pdef.CONTENT_2)
+       self.string_to_content(YAML_1, CONTENT_1)
+       self.string_to_content(YAML_2, CONTENT_2)
        
     def test_attributes(self):
        z = pdef.StringAsYAML(MINIMAL)
@@ -77,8 +137,8 @@ class Test_ParsingDefintion(unittest.TestCase):
         return fp.name    
     
     def setUp(self):
-        self.filename1 = self.make_file(pdef.YAML_1)
-        self.filename2 = self.make_file(pdef.YAML_2)
+        self.filename1 = self.make_file(YAML_1)
+        self.filename2 = self.make_file(YAML_2)
         self.filename3 = self.make_file(EMPTY)       
         
     def tearDown(self):    
@@ -90,8 +150,8 @@ class Test_ParsingDefintion(unittest.TestCase):
         assert pdef.ParsingDefinition(path=_filename).content == _content       
         
     def test_string_to_file_to_content(self):       
-        self.filename_to_content(self.filename1, pdef.CONTENT_1)
-        self.filename_to_content(self.filename2, pdef.CONTENT_2)
+        self.filename_to_content(self.filename1, CONTENT_1)
+        self.filename_to_content(self.filename2, CONTENT_2)
         self.filename_to_content(self.filename3, EMPTY_CONTENT)
 
 class Test_Get_Definitions(unittest.TestCase):
@@ -106,4 +166,4 @@ class Test_Get_Definitions(unittest.TestCase):
             assert isinstance(p, pdef.ParsingDefinition)
 
 if __name__ == '__main__':
-    unittest.main()     
+    unittest.main()
