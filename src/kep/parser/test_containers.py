9a823ca0c40cffd3cce6a51fe0fbed4de04d99de
E Pogrebnyak <e.pogrebnyak@gmail.com>
2017-05-09 12:37:49 +0300

    moved files around

--- src/kep/parser/test_containers.py
+++ src/kep/parser/test_containers.py
@@ -1,5 +1,19 @@
 import unittest
 
-class Test_Containers_No_Tests_Yet(unittest.TestCase):
-    def test_fail(self):
-        assert False
+import kep.containers as containers
+
+# most cases
+assert containers.get_year('2015') == 2015
+# some cells with comment
+assert containers.get_year('20161)') == 2016
+# some cells with two comments
+assert containers.get_year('20161)2)') == 2016
+# will not match with extra space
+assert containers.get_year(' 20161)2)') is None
+# not valid year
+assert containers.get_year('1. Сводные показатели') is None
+assert containers.get_year('27000,1-45000,0') is None
+
+
+if __name__ == '__main__':
+    unittest.main()
